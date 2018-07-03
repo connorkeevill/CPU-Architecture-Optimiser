d2ebc43705bbcaf3b6c749fba51dc55472d6c019
Connor Keevill <connor.keevill.19@gmail.com>
2018-07-03 10:34:40 +0100

    Add comments

--- CPU Architecture Optimiser/Program.cs
+++ CPU Architecture Optimiser/Program.cs
@@ -4,34 +4,51 @@ namespace CPU_Architecture_Optimiser
 {
     internal class Program
     {
-        public static void Main(string[] args) {
+        public static void Main() {
+            // | The length for the arrays to be
             int arrayLength = 100000000;
+
+            // | Create the arrays
             int[] unsorted = createOrderedArray(arrayLength);
             int[] sorted = createOrderedArray(arrayLength);
 
+            // | Shuffle the array to be 'unsorted'
             unsorted = shuffleArray(unsorted);
 
+            // | Begin adding the numbers in the unsorted array
             Console.WriteLine("Unsorted Array starting now:");
             DateTime unsortedTimeBefore = DateTime.Now;
             int unsortedSum = addEvens(unsorted);
             DateTime unsortedTimeAfter = DateTime.Now;
             TimeSpan unsortedTimeTaken = unsortedTimeAfter - unsortedTimeBefore;
 
+            // | Output time taken to process the unsorted array
             Console.WriteLine("Time taken for unsorted array: " + unsortedTimeTaken.TotalMilliseconds.ToString());
 
+            // | Begin adding the numbers in the sorted array
             Console.WriteLine("Sorted Array starting now: ");
             DateTime sortedTimeBefore = DateTime.Now;
             int sortedSum = addEvens(sorted);
             DateTime sortedTimeAfter = DateTime.Now;
             TimeSpan sortedTimeTaken = sortedTimeAfter - sortedTimeBefore;
 
+            // | Output the time taken to process the sorted array
             Console.WriteLine("Time taken for sorted array: " + sortedTimeTaken.TotalMilliseconds.ToString());
         }
 
+        // | isEven()
+        // |-------------------------------------------------------------
+        // | Returns a flag indicating whether or not a number is even.
+        // |--------------------------------------------------------
         private static bool isEven(int number) {
             return (number % 2 == 0);
         }
 
+        // | addEvens()
+        // |---------------------------------------------------
+        // | Iterates through the passed array and calculates
+        // | the sum of all the even numbers in the array.
+        // |-------------------------------------------
         private static int addEvens(int[] numbers) {
             int total = 0;
 
@@ -43,6 +60,10 @@ namespace CPU_Architecture_Optimiser
             return total;
         }
 
+        // | shuffleArray()
+        // |----------------------------------
+        // | Mixes up an array of integers.
+        // |---------------------------
         private static int[] shuffleArray(int[] array)
         {
             Random r = new Random();
@@ -56,6 +77,10 @@ namespace CPU_Architecture_Optimiser
             return array;
         }
 
+        // | createOrderedArray()
+        // |----------------------------------------------------------
+        // | Creates an array of a specified size, which is ordered.
+        // |----------------------------------------------------
         private static int[] createOrderedArray(int arraySize) {
             int[] array = new int[arraySize];
 
@@ -66,14 +91,22 @@ namespace CPU_Architecture_Optimiser
             return array;
         }
 
+        // | printArray()
+        // |---------------------------------------
+        // | Iterates through an array an outputs
+        // | each item's value to the console.
+        // |-------------------------------
         private static void printArray(int[] array) {
             foreach (int item in array) {
                 Console.WriteLine(item.ToString());
             }
         }
 
-        private static int[] createRandomArray() {
-            int arraySize = 100000000;
+        // | createRandomArray()
+        // |-------------------------------------------------------------
+        // | Creates an array of random numbers, of a specified amount.
+        // |-------------------------------------------------------
+        private static int[] createRandomArray(int arraySize) {
             int[] array = new int[arraySize];
 
             Random randSeed = new Random();
