e84ce448bdd294660c3b16b78f273a6354f4fc07
Connor Keevill <connor.keevill.19@gmail.com>
2018-07-17 22:35:31 +0100

    Finish renaming

--- Speculative Execution Demonstration/Properties/AssemblyInfo.cs
+++ Speculative Execution Demonstration/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CPU_Architecture_Optimiser")]
+[assembly: AssemblyTitle("Speculative_Execution_Demonstration")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CPU_Architecture_Optimiser")]
+[assembly: AssemblyProduct("Speculative_Execution_Demonstration")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
